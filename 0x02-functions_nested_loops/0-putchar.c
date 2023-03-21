1bc41550b12c43a2cd6bddeda78feed09d701465
AymanSaadi <aymansaadi2001pro@gmail.com>
2023-03-21 09:59:25 -0700

    eza

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Write a program that prints
- *
- * Description: program will print putchar
- * 
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Description: Prints' _putchar to stdout'
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("_putchar");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
