897ca610e767ea4b124c909b034f66b8aeacbc78
AymanSaadi <aymansaadi2001pro@gmail.com>
2023-03-21 06:08:49 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints
+ *
+ * Description: program will print putchar
+ * 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
